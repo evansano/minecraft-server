3ff409af7b6dc6089d53e1389ff70f01d8c22f0a
evansano <evanksano@gmail.com>
2020-11-28 11:11:32 -0800

    Initialize basic EC2 terraform structure and setup initial variable values to be used by various resources

---
+++ terraform/vars.tf
@@ -0,0 +1,20 @@
+# Using Ubuntu 18.04 general purpose starting point
+variable "AMI" {
+    type = string
+    default = ["ami-0a741b782c2c8632d"]
+}
+
+variable "REGION" {
+    type = string
+    default = ["us-west-1"]
+}
+
+variable "SERVER_PORT" {
+    type = number
+    default = [25565]
+}
+
+variable "DOMAIN_NAME" {
+    type = string
+    default = ["evansano.com"]
+}
