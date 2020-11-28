a48695b5495befd7c1b0bbe7493abffd8fac3efc
evansano <evanksano@gmail.com>
2020-11-28 11:14:55 -0800

    Add empty provider file and fix vars syntax

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -1,20 +1,20 @@
 # Using Ubuntu 18.04 general purpose starting point
 variable "AMI" {
     type = string
-    default = ["ami-0a741b782c2c8632d"]
+    default = "ami-0a741b782c2c8632d"
 }
 
 variable "REGION" {
     type = string
-    default = ["us-west-1"]
+    default = "us-west-1"
 }
 
 variable "SERVER_PORT" {
     type = number
-    default = [25565]
+    default = 25565
 }
 
 variable "DOMAIN_NAME" {
     type = string
-    default = ["evansano.com"]
+    default = "evansano.com"
 }
